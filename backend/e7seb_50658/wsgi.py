479672eb7a1909162eb449d68c9b7f3b39ef0b7e
Crowdbotics <team@crowdbotics.com>
2025-01-13 18:02:17 +0000

    Initial commit

---
+++ backend/e7seb_50658/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for e7seb_50658 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "e7seb_50658.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "e7seb-50658",
+  url: "https://e7seb-50658.botics.co",
+  api: "https://e7seb-50658.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
